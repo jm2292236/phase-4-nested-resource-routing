fcf95e6fb0675c9705db3b4c01ff5364a62b5b92
Jaime Montes de Oca <jaime0422@live.com>
2022-08-28 16:50:28 -0700

    Completed Assignment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,35 @@
 class ReviewsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
-    reviews = Review.all
-    render json: reviews, include: :dog_house
-  end
+    def index
+        if params[:dog_house_id]
+            dog_house = DogHouse.find(params[:dog_house_id])
+            reviews  = dog_house.reviews
+        else
+            reviews = Review.all
+        end
 
-  def show
-    review = Review.find(params[:id])
-    render json: review, include: :dog_house
-  end
+        render json: reviews, include: :dog_house
+    end
 
-  def create
-    review = Review.create(review_params)
-    render json: review, status: :created
-  end
+    def show
+        review = Review.find(params[:id])
+        render json: review, include: :dog_house
+    end
 
-  private
+    def create
+        review = Review.create(review_params)
+        render json: review, status: :created
+    end
 
-  def render_not_found_response
-    render json: { error: "Review not found" }, status: :not_found
-  end
+    private
 
-  def review_params
-    params.permit(:username, :comment, :rating)
-  end
+    def review_params
+        params.permit(:username, :comment, :rating)
+    end
+
+    def render_not_found_response
+        render json: { error: "Review not found" }, status: :not_found
+    end    
 
 end
